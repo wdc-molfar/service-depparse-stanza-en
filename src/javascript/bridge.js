9ddea22e6e49dbd2be16e9025b28731f221e304b
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2024-03-14 20:40:13 +0200

    Comment example

--- src/javascript/bridge.js
+++ src/javascript/bridge.js
@@ -17,25 +17,25 @@ const DepParse = class extends Bridge {
 
 module.exports = DepParse
 
-const run = async () => {
-
-	const config = {
-		mode: 'text',
-		encoding: 'utf8',
-		pythonOptions: ['-u'],
-		pythonPath: (process.env.NODE_ENV && process.env.NODE_ENV === "production") ? "python" : "python.exe",
-		args: `en ${path.resolve(__dirname, "./Stanza-models")}`
-	}
-
-	const extractor = new DepParse(config)
-	extractor.start()
-
-	const text = `United States of America is a country. Barack Obama was a president of it. His daughters are Sasha and Malia.`
-	console.log(text)
-	let res = await extractor.getDepParse(text)
-
-	console.log(res)
-	extractor.terminate()
-}
-
-run()
+// const run = async () => {
+//
+// 	const config = {
+// 		mode: 'text',
+// 		encoding: 'utf8',
+// 		pythonOptions: ['-u'],
+// 		pythonPath: (process.env.NODE_ENV && process.env.NODE_ENV === "production") ? "python" : "python.exe",
+// 		args: `en ${path.resolve(__dirname, "./Stanza-models")}`
+// 	}
+//
+// 	const extractor = new DepParse(config)
+// 	extractor.start()
+//
+// 	const text = `United States of America is a country. Barack Obama was a president of it. His daughters are Sasha and Malia.`
+// 	console.log(text)
+// 	let res = await extractor.getDepParse(text)
+//
+// 	console.log(res)
+// 	extractor.terminate()
+// }
+//
+// run()
