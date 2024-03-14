64fea54b319f489ae73878a85c90b858ec9bff8a
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2024-03-14 23:24:48 +0200

    Make response compatible with Apache ECharts

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # service-depparse-stanza-en
 Stanza Dependency Parser for English Language Molfar Service
+
+Returned data is compatible with [Apache ECharts](https://echarts.apache.org/):
+```javascript
+const dom = document.getElementById('chart-container');
+
+const myChart = echarts.init(dom, null, {
+  renderer: 'canvas',
+  useDirtyRect: false
+});
+
+const inputText = '(any text from which you need to extract dependencies)';
+const graph = await extract_dependencies(inputText); // get response from depparse here
+
+const option = {
+  legend: [
+    {
+      data: graph.categories.map(function (a) {
+        return a.name;
+      })
+    }
+  ],
+  series: [
+    {
+      type: 'graph',
+      layout: 'force',
+      edgeSymbol: ['none', 'arrow'],
+      data: graph.nodes,
+      links: graph.links,
+      categories: graph.categories,
+      roam: true,
+      label: {
+        show: true
+      },
+      edgeLabel: {
+        show: true,
+        formatter: '{@value}'
+      },
+      force: {
+        repulsion: 100
+      }
+    }
+  ]
+};
+myChart.setOption(option);
+```
--- src/python/depparse.py
+++ src/python/depparse.py
@@ -10,6 +10,29 @@ import stanza
 input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
 
 
+# Universal POS tags from https://universaldependencies.org/u/pos/
+def get_possible_upos_tags():
+    return [
+        "ADJ",      # adjective
+        "ADP",      # adposition
+        "ADV",      # adverb
+        "AUX",      # auxiliary
+        "CCONJ",    # coordinating conjunction
+        "DET",      # determiner
+        "INTJ",     # interjection
+        "NOUN",     # noun
+        "NUM",      # numeral
+        "PART",     # particle
+        "PRON",     # pronoun
+        "PROPN",    # proper noun
+        "PUNCT",    # punctuation
+        "SCONJ",    # subordinating conjunction
+        "SYM",      # symbol
+        "VERB",     # verb
+        "X"         # other
+    ]
+
+
 if __name__ == '__main__':
     model_lang = sys.argv[1]
     model_dir = sys.argv[2]
@@ -26,21 +49,38 @@ if __name__ == '__main__':
         try:
             input_json = json.loads(line)
             method = input_json['method']
-            text = input_json['params']['text']
-            output = None
-            doc = nlp(text)
-            if method == 'extract_dependencies':
-                output = []
-                for sentence in doc.sentences:
-                    for (source_word, dependency, target_word) in sentence.dependencies:
-                        if dependency == 'root':
-                            continue
-                        output.append(dict(
-                            source=source_word.lemma,
-                            dependency=dependency,
-                            target=target_word.lemma
-                        ))
-                output = {"request": input_json, "response": {"dependencies": output}}
+
+            if method == 'get_possible_upos_tags':
+                tags = get_possible_upos_tags()
+                output = {"request": input_json, "response": {"tags": tags}}
+            else:
+                text = input_json['params']['text']
+                output = None
+                doc = nlp(text)
+                if method == 'extract_dependencies':
+                    nodes = []
+                    links = []
+                    categories = get_possible_upos_tags()
+                    upos_to_category_id_dict = {k: v for v, k in enumerate(categories)}
+                    categories = [{"name": category} for category in categories]
+                    output = []
+                    for sent_id, sentence in enumerate(doc.sentences, start=1):
+                        for word in sentence.words:
+                            nodes.append(dict(
+                                id=f'{sent_id}-{word.id}',
+                                name=word.lemma,
+                                value=word.text,
+                                category=upos_to_category_id_dict[word.upos]
+                            ))
+                        for (source_word, dependency, target_word) in sentence.dependencies:
+                            if dependency == 'root':
+                                continue
+                            links.append(dict(
+                                source=f'{sent_id}-{source_word.id}',
+                                value=dependency,
+                                target=f'{sent_id}-{target_word.id}'
+                            ))
+                    output = {"request": input_json, "response": dict(nodes=nodes, links=links, categories=categories)}
         except BaseException as ex:
             ex_type, ex_value, ex_traceback = sys.exc_info()
 
